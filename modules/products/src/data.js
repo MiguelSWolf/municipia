cd61e42f7f897507ec60fd78c023ab6b41f92b55
MiguelSWolf <soulmiguel@gmail.com>
2021-01-29 09:15:54 -0300

    melhorando os dados estaticos

--- modules/products/src/data.js
+++ modules/products/src/data.js
@@ -1,12 +1,12 @@
 export default [
-    { id: "1", name: "Toyota Yaris", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Renault Captur", value: 2000, year: 2016, owner_id: "1" }, 
-    { id: "1", name: "Ford Focus", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Skoda Octavia", value: 2000, year: 2016, owner_id: "1" }, 
-    { id: "1", name: "Nissan Qashqai", value: 2000, year: 2016, owner_id: "1" }, 
-    { id: "1", name: "Peugeot 208", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Ford Fiesta", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Volkswagen Polo", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Renault Clio", value: 2000, year: 2016, owner_id: "1" } ,
-    { id: "1", name: "Volkswagen Golf", value: 2000, year: 2016, owner_id: "1" } 
+    { id: "1", name: "Toyota Yaris", value: 1824, year: 2019, owner_id: "1" } ,
+    { id: "2", name: "Renault Captur", value: 2408, year: 2018, owner_id: "2" }, 
+    { id: "3", name: "Ford Focus", value: 1389, year: 2014, owner_id: "3" } ,
+    { id: "4", name: "Skoda Octavia", value: 2146, year: 2015, owner_id: "4" }, 
+    { id: "5", name: "Nissan Qashqai", value: 1953, year: 2018, owner_id: "1" }, 
+    { id: "6", name: "Peugeot 208", value: 2483, year: 2019, owner_id: "3" } ,
+    { id: "7", name: "Ford Fiesta", value: 1483, year: 2016, owner_id: "2" } ,
+    { id: "8", name: "Volkswagen Polo", value: 1103, year: 2014, owner_id: "4" } ,
+    { id: "9", name: "Renault Clio", value: 1376, year: 2015, owner_id: "2" } ,
+    { id: "10", name: "Volkswagen Golf", value: 1007, year: 2017, owner_id: "1" } 
 ];
